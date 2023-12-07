1b39bb3f0d228b05768577c48f897b224da5cc46
ElodieWan <elodiewan2001@gmail.com>
2023-12-07 21:01:07 +0100

    Feat: first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div class="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { firebaseConfig } from './services/firebase.config';
+import {initializeApp} from 'firebase/app';
+
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export default app 
